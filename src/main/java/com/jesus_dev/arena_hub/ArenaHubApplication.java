d0c7e62eeb120a8bac49ed3c6f485a343e625e05
ImJesusGomez <jesusgomez.dev10@gmail.com>
2026-06-09 09:43:00 -0600

    first commit

---
+++ src/main/java/com/jesus_dev/arena_hub/ArenaHubApplication.java
@@ -0,0 +1,13 @@
+package com.jesus_dev.arena_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenaHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesus_dev/arena_hub/ArenaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus_dev.arena_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
